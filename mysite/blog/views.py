8c65b61000f6ea9271344df3b249c759043becc0
zuhara <zuhara@hamon.in>
2019-09-30 16:01:13 +0530

    Finishes the blog with forms

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,7 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from .models import Post
+from django.utils import timezone
+from .forms import PostForm
 
 # Create your views here.
 
 def post_list(request):
 	context={}
+	posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+	context['posts'] = posts
 	return render(request,'blog/post_list.html',context)
+
+def post_new(request):
+	if request.method == "POST":
+		form = PostForm(request.POST)
+		if form.is_valid():
+			post = form.save(commit=False)
+			post.author = request.user
+			post.published_date = timezone.now()
+			post.save()
+			return redirect('post_list')
+	else:
+		form = PostForm()
+		return render(request, 'blog/post_new.html',{'form':form})
