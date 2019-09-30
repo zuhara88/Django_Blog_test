42a28a8408dc72332a7c86bf248320e082c4ca2b
zuhara <zuhara@hamon.in>
2019-09-30 11:53:41 +0530

    Initial Commit

---
+++ mysite/blog/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+from django.conf import settings
+from django.utils import timezone
+
+class Post(models.Model):
+	title = models.CharField(max_length=200)
+	text = models.TextField()
+	author = models.ForeignKey(settings.AUTH_USER_MODEL,on_delete=models.CASCADE)
+	created_date = models.DateTimeField(blank=True, null=True)
+	published_date = models.DateTimeField(blank=True, null=True)
+
+	def publish(self):
+		self.published_date=timezone.now()
+		self.save()
+
+	def __str__(self):
+		return self.title
+
+
